73b87799a74e4dafaa6203c56f618de0448d4e79
zhaojy <zhaojunyang@anzhi.com>
2012-09-04 11:35:15 +0800

    adf

--- TabHostDemo/src/test/tabhost/demo/ActivityOne.java
+++ TabHostDemo/src/test/tabhost/demo/ActivityOne.java
@@ -17,8 +17,7 @@ public class ActivityOne extends Activity {
 		
 		output.setText("第一个Activity");
 		
-		Log.e("aa", "另外一个clone内做的改动");
-		Log.e("aa", "第一个分支新增");
+		Log.e("aa", "第二个分支进行修改");
 		
 	}
 	
